ae2319f5724401ce890fbfbffaea59ca560950bf
Pamela <contatopamela@outlook.com>
2024-07-11 19:38:35 -0300

    Criação do Menu

---
+++ Menu.ts
@@ -0,0 +1,96 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/colors";
+
+
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log(colors.bg.blue, colors.fg.gray,
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                Livraria Sonhos                      ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Listar todos os Produtos             ");
+        console.log("            2 - Listar Produto pelo ID               ");
+        console.log("            3 - Cadastrar Produto                    ");
+        console.log("            4 - Atualizar Produto                    ");
+        console.log("            5 - Deletar Produto                      ");
+        console.log("            6 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", 
+        colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao === 6) {
+            console.log(colors.fg.greenstrong,
+                "\nLivraria Sonhos - Todas as opções de livros em um só lugar!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        console.log(colors.fg.whitestrong)
+
+        switch(opcao){
+            case 1:
+                console.log("\n\nCadastrar Produto\n\n")
+
+                keyPress()
+                break
+            case 2: 
+                console.log("\n\nListar todos Produtos\n\n")
+
+                keyPress()                
+                break
+            case 3:
+                console.log("\n\nBuscar produto por ID\n\n")
+                keyPress()
+                break
+            case 4:
+                console.log("\n\nAtualizar Produto\n\n")
+
+                keyPress()
+                break
+            case 5:
+                console.log("\n\nApagar Produto\n\n")
+
+                keyPress()
+                break
+
+            default:
+                console.log(colors.fg.red)
+                console.log("\n\nOpção Inválida!\n\n")
+                console.log(colors.reset)
+                keyPress()
+        }
+
+
+    }
+}
+
+
+/* Função com os dados da pessoa desenvolvedora */
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: Pamela Salgado");
+    console.log("E-mail - contatopamela@outlook.com");
+    console.log("https://github.com/pamelalmeidaa");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
